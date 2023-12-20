ef26f73e991ed53616521a7d4c243df6d4d25d86
Thenebit <napaasamuel5@gmail.com>
2023-12-19 19:55:41 -0500

    Authentication Added

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,13 @@
 
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/', [PostController::class, 'index']);
 
